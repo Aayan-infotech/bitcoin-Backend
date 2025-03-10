f652f721a1ca93f095272a58a5c8d54b21f03316
anurag721721 <anurag.yadav@aayaninfotech.com>
2025-03-10 17:40:13 +0530

    add change

--- models/userModel.js
+++ models/userModel.js
@@ -16,8 +16,8 @@ const UserSchema = new Schema(
     },
     userType: {
       type: String,
-      enum: ["User", "Admin"],
-      default: "User",
+      enum: ["Student", "Salaried","Admin","Self Employed"],
+      default: "Student",
     },
     image: { type: String },
     walletAddress: {
