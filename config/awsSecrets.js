7e35b553abf00d413146a568f7c9ab86c4923b5b
Kartikey Tiwari <123266503+Codewallahh@users.noreply.github.com>
2025-08-21 19:33:32 +0530

    Update awsSecrets.js

--- config/awsSecrets.js
+++ config/awsSecrets.js
@@ -3,7 +3,7 @@ const {
   GetSecretValueCommand
 } = require("@aws-sdk/client-secrets-manager");
 
-const secretName ="bit-vault";
+const secretName ="bitcoin-secret";
 const region ="us-east-1"; 
 
 let cachedSecrets = null;
